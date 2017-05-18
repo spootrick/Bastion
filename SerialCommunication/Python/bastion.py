e9a83002a080ddc91d3424aa4bf85a22b6a135f7
Spootrick <myoiatr@gmail.com>
2017-05-18 11:00:50 +0300

    added turn movements for python

--- SerialCommunication/Python/bastion.py
+++ SerialCommunication/Python/bastion.py
@@ -13,20 +13,30 @@ $$    $$/ $$    $$ |/     $$/   $$  $$/ $$ |$$    $$/ $$ |  $$ |
 $$$$$$$/   $$$$$$$/ $$$$$$$/     $$$$/  $$/  $$$$$$/  $$/   $$/
                                                     The Sentinel
 
+Type "h" for help.
+
+Type "w" for move forward.
+Type "s" for move backward.
+Type "a" for turn left.
+Type "d" for turn right.
+
+Type "space" for stop!
+
 """)
 
 try:
-  serial_connection = serial.Serial('/dev/ttyACM0', 115200)
+  serial_connection = serial.Serial('/dev/ttyACM0', 9600)
 except:
   print('Error: Cannot connected to the arduino!')
   sys.exit()
 
 while 1:
-  n = input('Enter a command: (type h for help)')
+  n = input('Enter a command: ')
   command = str(n)
 
   if command == "h":
     print("""
+
 Bastion Help Page
 -------------------------
 w - move forward
@@ -44,10 +54,14 @@ space - stop
   elif command == " ":
     serial_connection.write(bytes('0', 'UTF-8'))
     print("Stop!")
+  elif command == "a":
+    serial_connection.write(bytes('3', 'UTF-8'))
+    print("Turning left..")
+  elif command == "d":
+    serial_connection.write(bytes('4', 'UTF-8'))
+    print("Turning right..")
   elif command == "q":
     print("exit!")
     sys.exit()
   else:
     print("Typed wrong command! Press h for help.")
-
-
