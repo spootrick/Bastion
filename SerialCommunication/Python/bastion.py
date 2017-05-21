fd2e39e05d383778b01bbc4d35cbdaabf4fb5990
Spootrick <myoiatr@gmail.com>
2017-05-21 13:52:49 +0300

    rewrited script with using curses

--- SerialCommunication/Python/bastion.py
+++ SerialCommunication/Python/bastion.py
@@ -1,63 +1,124 @@
-import serial
-import sys
+#!/usr/bin/env python
+# -- coding: utf-8 --
+# File              : bastion.py
+# Description       : Serial connection script for arduino
+# Author            : Furkan Karakoyunlu
+# Date              : 21.05.2017
+# Python version    : 3.4
+# =======================================
 
-print("""
-
-/  |                            /  |    /  |
-$$ |____    ______    _______  _$$ |_   $$/   ______   _______
-$$      \  /      \  /       |/ $$   |  /  | /      \ /       \
-$$$$$$$  | $$$$$$  |/$$$$$$$/ $$$$$$/   $$ |/$$$$$$  |$$$$$$$  |
-$$ |  $$ | /    $$ |$$      \   $$ | __ $$ |$$ |  $$ |$$ |  $$ |
-$$ |__$$ |/$$$$$$$ | $$$$$$  |  $$ |/  |$$ |$$ \__$$ |$$ |  $$ |
-$$    $$/ $$    $$ |/     $$/   $$  $$/ $$ |$$    $$/ $$ |  $$ |
-$$$$$$$/   $$$$$$$/ $$$$$$$/     $$$$/  $$/  $$$$$$/  $$/   $$/
-                                                    The Sentinel
-
-Type "h" for help.
-
-""")
+import sys,os
 
 try:
-  serial_connection = serial.Serial('/dev/ttyACM0', 9600)
-except:
-  print('Error: Cannot connected to the arduino!')
-  sys.exit()
+    import curses
+except ImportError:
+    raise ImportError("The library 'curses' couldn't imported")
 
-while 1:
-  n = input('Enter a command: ')
-  command = str(n)
+try:
+    import serial
+except ImportError:
+    raise ImportError("The library 'serial' couldn't imported")
 
-  if command == "h":
-    print("""
 
-Bastion Help Page
--------------------------
-w - move forward
-s - move backward
-a - turn left
-d - turn right
+def draw_screen_for_serial_connection(screen):
+    # do not echo keys to the screen
+    curses.noecho()
+    # send keys without pressing enter
+    curses.cbreak()
 
-Press "space" for stop!
+    try:
+      serial_connection = serial.Serial('/dev/ttyACM0', 9600)
+    except:
+      print('Error: Cannot connected to the arduino!')
+      sys.exit()
 
-""")
+    command = ''
+    k = 0
 
-  elif command == "w":
-    serial_connection.write(bytes('1', 'UTF-8'))
-    print("Moving forward.. ")
-  elif command == "s":
-    serial_connection.write(bytes('2', 'UTF-8'))
-    print("Moving backward.. ")
-  elif command == " ":
-    serial_connection.write(bytes('0', 'UTF-8'))
-    print("Stop!")
-  elif command == "a":
-    serial_connection.write(bytes('3', 'UTF-8'))
-    print("Turning left..")
-  elif command == "d":
-    serial_connection.write(bytes('4', 'UTF-8'))
-    print("Turning right..")
-  elif command == "q":
-    print("exit!")
-    sys.exit()
-  else:
-    print("Typed wrong command! Press h for help.")
+    cursor_x = 0
+    cursor_y = 0
+
+    # starting colors for curses
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    # looping the last pressed key k
+    while (k != ord('Q')):
+        # initialization
+        screen.clear()
+        height, width = screen.getmaxyx()
+
+        if k == ord('w'): # forward 200
+            serial_connection.write(bytes('1', 'UTF-8'))
+            command = 'FORWARD 200'
+        elif k == ord('s'): # backward 200
+            serial_connection.write(bytes('2', 'UTF-8'))
+            command = 'BACKWARD 200'
+        if k == ord('W'): # forward 400
+            serial_connection.write(bytes('3', 'UTF-8'))
+            command = 'FORWARD 400'
+        elif k == ord('S'): # backward 400
+            serial_connection.write(bytes('4', 'UTF-8'))
+            command = 'BACKWARD 400'
+        elif k == ord('a'): # turn left
+            serial_connection.write(bytes('5', 'UTF-8'))
+            command = 'TURN LEFT'
+        elif k == ord('d'): # turn right
+            serial_connection.write(bytes('6', 'UTF-8'))
+            command = 'TURN RIGHT'
+        elif k == ord(' '): # stop
+            serial_connection.write(bytes('0', 'UTF-8'))
+            command = 'STOP'
+
+        # declare the strings
+        title = "Bastion The Sentinel Controller"[:width-1]
+        subtitle = "Written by spootrick"[:width-1]
+        keystr = "Last command: {}".format(command)[:width-1]
+        helpstr = "w -> forward, s -> backward, a -> left, d -> right, space -> stop"
+        statusbarstr = "Press 'Q' to exit | BASTION CONTROLLER"
+
+        if command == '':
+            keystr = "waiting for command ..."[:width-1]
+
+        # text centering
+        start_x_title = int((width // 2) - (len(title) // 2) - len(title) % 2)
+        start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
+        start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
+        start_y = int((height // 2) - 5)
+
+        # render status bar
+        screen.attron(curses.color_pair(2))
+        screen.addstr(height-1, 0, statusbarstr)
+        screen.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+        screen.attroff(curses.color_pair(2))
+
+        # turning on attributes for title
+        screen.attron(curses.color_pair(1))
+        screen.attron(curses.A_BOLD)
+
+        # render title
+        screen.addstr(start_y, start_x_title, title)
+
+        # turning off attributes for title
+        screen.attroff(curses.color_pair(1))
+        screen.attroff(curses.A_BOLD)
+
+        # printing the rest of the text
+        screen.addstr(start_y + 1, start_x_subtitle, subtitle)
+        screen.addstr(start_y + 3, (width // 2) - 6, '* * * * *')
+        screen.addstr(start_y + 5, start_x_keystr, keystr)
+        screen.addstr(start_y + 13, (width // 2) - 35 , helpstr)
+        screen.move(cursor_y, cursor_x)
+
+        # refresh screen
+        screen.refresh()
+
+        # wait for next input
+        k = screen.getch()
+
+def main():
+    curses.wrapper(draw_screen_for_serial_connection)
+
+if __name__ == "__main__":
+    main()
